7d2ab87429d103588bbf3ed2c05308a0cf0cf8df
an2210sh <anshsriv.2001@gmail.com>
2026-02-25 09:48:39 +0530

    var

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+// console.log("Hello World");
+// var ko ham redeclare aur reassign again 
+var num=10;
+console.log(num);
+console.log(typeof(num));  //output : number
+num="Ansh";
+console.log(num);
+console.log(typeof(num));  //string
+num=true;
+console.log(num);
+console.log(typeof(num));  // boolean
+num=null;
+console.log(num);
+
+console.log(typeof(num));  
+num=undefined;
+console.log(num);
+console.log(typeof(num));   
