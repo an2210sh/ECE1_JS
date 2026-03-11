3db1937f2dc2da03e6187f6db41e7d0038983c38
an2210sh <anshsriv.2001@gmail.com>
2026-03-11 09:44:41 +0530

    BASICS OF JS

---
+++ const.js
@@ -0,0 +1,21 @@
+// const sym1=Symbol(4);
+// console.log(sym1);
+// const sym2=Symbol(4);
+// if(sym1==sym2){
+//     console.log("Yes");
+// }else{
+//     console.log("no");
+// }
+// var a=20;
+// var b=20;
+    // var count;
+    // document.write("Starting Loop"+"<br/>");
+    // for(count=0;count<10;count++){
+    //     document.write("Current Count : "+count);
+    //     document.write("<br/>");
+    // }
+    // document.write("Loop Stopped");
+    var age=20;
+if(age>18){
+   document.write("<b>You are eligible to vote<b/>");
+   }
